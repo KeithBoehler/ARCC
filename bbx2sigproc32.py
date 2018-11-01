5bf8e3c31618a3ca69f6ed81aeb4779fc24818af
Keith Boehler <germanbohler@gmail.com>
2018-11-01 17:52:22 -0500

    I fixed problems i was having with the argparse. Now i am able to give input path from terminal with --path

--- bbx2sigproc32.py
+++ bbx2sigproc32.py
@@ -15,6 +15,10 @@ import warnings
 from psr_constants import ARCSECTORAD
 import sigproc
 from filterbank import create_filterbank_file as filMake
+import argparse 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--path", help="Path to input bbx file.")
 
 lowFreq = 25.0
 highFreq = 75.0
@@ -23,16 +27,16 @@ lowBin = pdat.freq2bin(lowFreq)
 highBin = pdat.freq2bin(highFreq)
 
 # Open file to translate 
-#lofasmFile = raw_input("Type name of file? ")
-#lofasmFile = "20160928_060014_AA.bbx.gz"
-lofasmFile =  'catfile.bbx'#"20170209_131819_AA.bbx"  #Last used
-#lf = bbx.LofasmFileClass(lofasmFile)
-lf = bbx.LofasmFile(lofasmFile)
+lofasmFile = vars(parser.parse_args())
+inPath = lofasmFile['path'] # bring out the path from dict to str
+print lofasmFile
+lf = bbx.LofasmFile(inPath)
 lf.read_data()
 # Get the file header dictionary 
 lofasmHeader = lf.header
 print lofasmHeader
-data = lf.data[lowBin:highBin, :].astype(np.float32)
+data = lf.data[:,lowBin:highBin].astype(np.float32) # uncomment 9/27/2018
+#data = lf.data[lowBin:highBin, :].astype(np.float32) # uncomment 8/6/2018 # recomment at above uncomment
 #data = lf.data.astype(np.float32)
 #new filterbank file name
 filName = 'newlofasm32.fil'
@@ -43,24 +47,24 @@ tsamp = float(lofasmHeader['dim1_span']) / float(lofasmHeader['metadata']['dim1_
 # The change here is that timebins seems to have been renamed.
 # There is a dictionary inside a dictionary, so we need those two [][]
 nsamples = int(lofasmHeader['metadata']['dim1_len']) # swaped out 'timebins'
-
+print "The value of dim 1 len is: {}".format(nsamples)
 rbw = float(lofasmHeader['dim2_span'])*1e-6 / float(lofasmHeader['metadata']['dim2_len']) #resolution bandwith. Changed freqbins to lofasmHeader['metadata']['dim2_len']
 print "rbw: {}".format(rbw)
 fch1 = highFreq - (rbw/2)
-
+print "The shape of the data is: {}".format(data.shape)
 #populate new header
 newHeader = {
-    'telescope_id': 1, # we are using the number one for now to say we are using AO
+    'telescope_id': 6, # we are using the number one for now to say we are using AO. 6 for GBT
     'machine_id': 0,
     'data_type': 1,
-    'rawdatafile': lofasmFile,
+    'rawdatafile': inPath,
     'tstart': tstart,
     'tsamp': tsamp,
     'nbits': 32,
     'nsamples': nsamples,
     'fch1': fch1,
     'foff': -1* rbw, #descending frequency channels
-    'nchans': (highBin-lowBin),
+    'nchans': highBin-lowBin,
     'nifs': 1,
     'source_name': 'keith',
     'Telescope': 'GBT',
@@ -74,7 +78,9 @@ for k in newHeader.keys():
 fbfile = filMake(filName, newHeader, nbits=32, verbose=True)
 
 for i in range(nsamples):
-    spectra = data[:, i][::-1]
+    spectra = data[i,:][::-1]#spectra = data[:,i][::-1]
+    #print "The spectra shape is: {}".format(spectra.shape)
+    #spectra = np.reshape(spectra, (1, newHeader['metadata']['dim2_len'])) 
     spectra = np.reshape(spectra, (1, newHeader['nchans']))
     fbfile.append_spectra(spectra)
 
