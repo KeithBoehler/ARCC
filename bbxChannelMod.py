a1d3f0bcaa93651090367006f2445b10f23c6441
Keith Boehler <germanbohler@gmail.com>
2019-07-15 21:17:09 -0500

    Adding streming capability to the channle mod script. Seems to work with large files

--- bbxChannelMod.py
+++ bbxChannelMod.py
@@ -16,13 +16,29 @@ parser.add_argument("-p","--path", help="Path to input bbx file.")
 parser.add_argument("-f", "--freq", help="Set frequancy channel to be removed. In MHz.")
 parser.add_argument("-o", "--outfile", help="Name for file created.")
 
-#Open the file we want to mod
+'''
+Stream Data. Open the file we want to mod. 
+Prep the file we are to write too.
+'''
 pars_dict = vars(parser.parse_args())
+bbxFileHandel = bbx.LofasmFile(pars_dict['path'])
 bbxfile = pars_dict['path']
-bbxFileHandel  = bbx.LofasmFile(bbxfile)
-bbxFileHandel.read_data()
-data = bbxFileHandel.data[:, :].astype(np.float32)
+
 bbxFileHeader = bbxFileHandel.header
+outPath = os.getcwd()
+outPath = outPath + '/' + pars_dict['outfile']
+
+for i in range(int(bbxFileHeader['metadata']['dim1_len'])):
+    bbxFileHandel.read_data(1) # Read one line of data
+    spectra = bbxFileHandel.data[:, :] #.astype(np.float32)
+    spectra[:,pdat.freq2bin(float(pars_dict['freq']))] = 0
+    outFile = bbx.LofasmFile(outPath, bbxFileHeader, mode = 'write')
+    outFile.add_data(spectra)
+    outFile.write()    
+
+outFile.write()
+'''
+data = bbxFileHandel.data[:, :].astype(np.float32)
 
 # select frequancy to zero 
 userin = float(pars_dict['freq'])
@@ -30,7 +46,8 @@ freqbin = pdat.freq2bin(userin)
 
 # zero out that frequancy
 data[:, freqbin] = 0
-
+'''
+'''
 # write to new bbx file
 path = os.getcwd()
 path = path + '/' + pars_dict['outfile']
@@ -38,3 +55,5 @@ outFile = bbx.LofasmFile(path, bbxFileHeader, mode = 'write')
 outFile.add_data(data)
 outFile.write()
 outFile.close()
+
+'''
